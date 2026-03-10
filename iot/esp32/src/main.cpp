1ec8b8be1437eaf39a83a2810e15adf0f5b22ad6
ken125678 <168824282+ken125678@users.noreply.github.com>
2026-03-10 10:19:08 +0700

    Implement Wi-Fi connection and sensor data handling

--- iot/esp32/src/main.cpp
+++ iot/esp32/src/main.cpp
@@ -18,10 +18,137 @@
  * 4. apply command
  */
 
-void setup() {
-  // TODO: initialize serial, Wi-Fi, sensors, and relays
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+#include "secrets.h"
+#include "config.h"
+#include "sensors.h"
+
+unsigned long lastSend = 0;
+
+void connectWiFi()
+{
+    Serial.println("Connecting WiFi...");
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nWiFi connected");
 }
 
-void loop() {
-  // TODO: implement periodic sensor reading and command polling
+void sendSensorData(float temp, float hum, int light)
+{
+    if (WiFi.status() != WL_CONNECTED)
+        return;
+
+    HTTPClient http;
+
+    String url = String(API_BASE_URL) + "/sensor-data";
+
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+
+    StaticJsonDocument<200> doc;
+
+    doc["temperature"] = temp;
+    doc["humidity"] = hum;
+    doc["light"] = light;
+
+    String json;
+    serializeJson(doc, json);
+
+    int httpCode = http.POST(json);
+
+    Serial.println("Send data:");
+    Serial.println(json);
+    Serial.println("HTTP: " + String(httpCode));
+
+    http.end();
+}
+
+void getDeviceCommand()
+{
+    if (WiFi.status() != WL_CONNECTED)
+        return;
+
+    HTTPClient http;
+
+    String url = String(API_BASE_URL) + "/device-command";
+
+    http.begin(url);
+
+    int httpCode = http.GET();
+
+    if (httpCode == 200)
+    {
+        String payload = http.getString();
+
+        StaticJsonDocument<200> doc;
+        deserializeJson(doc, payload);
+
+        int led = doc["led"];
+        int fan = doc["fan"];
+
+        digitalWrite(LED_PIN, led);
+        digitalWrite(RELAY_PIN, fan);
+
+        Serial.println("Device command received");
+    }
+
+    http.end();
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(RELAY_PIN, OUTPUT);
+
+    dht.begin();
+
+    connectWiFi();
+}
+
+void loop()
+{
+    if (millis() - lastSend > SEND_INTERVAL)
+    {
+        float temperature = readTemperature();
+        float humidity = readHumidity();
+        int light = readLight();
+
+        // Validate sensor readings
+        if (isnan(temperature))
+        {
+            Serial.println("Failed to read temperature!");
+            lastSend = millis();
+            return;
+        }
+
+        if (isnan(humidity))
+        {
+            Serial.println("Failed to read humidity!");
+            lastSend = millis();
+            return;
+        }
+
+        Serial.println("----- SENSOR DATA -----");
+        Serial.println("Temp: " + String(temperature));
+        Serial.println("Humidity: " + String(humidity));
+        Serial.println("Light: " + String(light));  
+
+        sendSensorData(temperature, humidity, light);
+
+        getDeviceCommand();
+
+        lastSend = millis();
+    }
 }
